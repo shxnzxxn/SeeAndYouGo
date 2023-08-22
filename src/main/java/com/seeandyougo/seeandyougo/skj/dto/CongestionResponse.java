2eb8fa06efc2380d578a828dcaa9845be5f33b99
juntae <juntae6942@o.cnu.ac.kr>
2023-08-22 21:22:47 +0900

    timeTable define

---
+++ src/main/java/com/seeandyougo/seeandyougo/skj/dto/CongestionResponse.java
@@ -0,0 +1,15 @@
+package com.seeandyougo.seeandyougo.skj.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CongestionResponse {
+    private Integer capacity;
+    private Integer connected;
+}
