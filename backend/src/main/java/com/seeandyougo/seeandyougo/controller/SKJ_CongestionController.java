5dcc51f9938064f4758cbf104b12b5b823dd210e
skj4244 <skj4244@naver.com>
2023-08-28 18:15:16 +0900

    shin branch 생성

--- backend/src/main/java/com/seeandyougo/seeandyougo/controller/SKJ_CongestionController.java
+++ backend/src/main/java/com/seeandyougo/seeandyougo/controller/SKJ_CongestionController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins="http://localhost:3001")
